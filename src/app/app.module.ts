7a4bbf762a112b85cb339614c9f64cee14c0fc06
Jesus bossa <jesusbossa@protonmail.com>
2018-05-07 20:56:33 -0500

    app init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { IntroComponent } from './intro/intro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    IntroComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
