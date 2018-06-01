8f944261b72e1016fa83d8a9199e0d65047b439a
Jesus bossa <jesusbossa@protonmail.com>
2018-06-01 11:18:09 -0500

    stuff page finish

--- src/app/components/stuff/stuff-page/stuff-page.component.ts
+++ src/app/components/stuff/stuff-page/stuff-page.component.ts
@@ -12,4 +12,15 @@ export class StuffPageComponent implements OnInit {
   ngOnInit() {
   }
 
+  cosas = [
+      { id: 1, titulo: 'Camisas de gato', src: 'https://placem.at/places?w=100&h=180&random=some_seed'},
+      { id: 2, titulo: 'Cinturones de gato', src: 'https://placem.at/places?w=100&h=180&random=some_seed'},
+      { id: 3, titulo: 'Shorts de gato', src: 'https://placem.at/places?w=100&h=180&random=some_seed'},
+      { id: 4, titulo: 'Amuletos de gato', src: 'https://placem.at/places?w=100&h=180&random=some_seed'},
+      { id: 5, titulo: 'Zapatos de gato', src: 'https://placem.at/places?w=100&h=180&random=some_seed'},
+      { id: 6, titulo: 'Comida de gato', src: 'https://placem.at/places?w=100&h=180&random=some_seed'},
+      { id: 7, titulo: 'Pantalones de gato', src: 'https://placem.at/places?w=100&h=180&random=some_seed'},
+      { id: 8, titulo: 'Collares de gato', src: 'https://placem.at/places?w=100&h=180&random=some_seed'},
+      { id: 9, titulo: 'Simbolos de gato', src: 'https://placem.at/places?w=100&h=180&random=some_seed'},
+  ]
 }
