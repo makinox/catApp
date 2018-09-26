5e2d478f2f757581065bc203c02ba550b53b4b2c
Jesus bossa <jesusbossa@protonmail.com>
2018-09-25 20:04:28 -0500

    react

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'React'
+
+export default class extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Hola mundo</h1>
+            </div>
+        )
+    }
+}
