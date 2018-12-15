fc7a4fb87c307e922ab3b9777b92e9a045576d5d
Jesús Bossa <me@jesusbossa.com>
2018-12-15 18:00:11 -0500

    [IMPROVEMENT] Project minified

---
+++ src/components/index/index.js
@@ -0,0 +1,74 @@
+import React from 'react'
+
+import Presentation from '../presentation/presentation'
+import Container from '../containers/container-2'
+import img1 from '../../assets/hero/cat.svg'
+import './index.css'
+
+const RazaCard = () => (
+	<div className="index-raza-card">
+		<img src="https://picsum.photos/100/100" alt="Imagenes de razas de gatos" />
+		<a href="/">Gato siames</a>
+	</div>
+)
+
+const TypeCard = () => (
+	<div className="index-types-card">
+		<a href="/">Raza siames <span role="img" aria-label="Emoji section">😜</span></a>
+	</div>
+)
+
+export default () => (
+  <>
+    <div className="index-hero-container">
+      <div>
+        <h1>Todo sobre gatos</h1>
+        <p>
+          Todo sobre los mejores gatos que puedas encontrar.
+          Si eres una amante de los gatos, deberias revisar accesorios de gatos para ti y tu mascota.
+        </p>
+      </div>
+      <div className="index-hero-item">
+        <img src={img1} alt="Logo de catapp" />
+      </div>
+    </div>
+
+    <Container>
+      <Presentation
+        title="Razas de gatos"
+        text="Porque no ver razas de gaticos? seguro que encuentras alguno que no conozcas o una que otra cosa que no sepas"
+        img="https://picsum.photos/200/200"
+        alt="Imagenes de razas de gatos"
+      />
+
+      <div className="index-raza-item">
+
+        <RazaCard />
+        <RazaCard />
+        <RazaCard />
+        <RazaCard />
+
+      </div>
+    </Container>
+
+    <Container>
+      <div className="index-types-item">
+        <TypeCard />
+        <TypeCard />
+        <TypeCard />
+        <TypeCard />
+        <TypeCard />
+        <TypeCard />
+        <TypeCard />
+        <TypeCard />
+      </div>
+
+      <Presentation
+        title="Tipos de gatos"
+        text="Porque no ver razas de gaticos? seguro que encuentras alguno que no conozcas o una que otra cosa que no sepas"
+        img="https://picsum.photos/200/200"
+        alt="Imagenes de razas de gatos"
+      />
+    </Container>
+  </>
+)
