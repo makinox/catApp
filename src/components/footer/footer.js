866427315b2f574ad836a105caf7b1d2086c0841
Jesús Bossa <me@jesusbossa.com>
2018-12-15 19:45:36 -0500

    [Improvement] Footer added

---
+++ src/components/footer/footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './footer.css'
+export default () => (
+  <footer>
+    <h2>2018 - <a href="https://twitter.com/jesMakinox" rel="noopener noreferrer" target="_blank">Jesús Bossa</a></h2>
+  </footer>
+)
