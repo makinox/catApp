3cd3bf456932931d389509bb3bcc8f5f4df98665
Jesús Bossa <me@jesusbossa.com>
2018-12-18 19:18:19 -0500

    [IMPROVEMENT] Raza cards added

--- src/components/raza/raza.js
+++ src/components/raza/raza.js
@@ -2,13 +2,44 @@ import React from 'react'
 import './raza.css'
 import Img1 from '../../assets/raza/cat3.svg'
 
-export default () => (
-  <div className="raza-header">
+const RazaCard = () => (
+  <div className="raza-card">
     <div>
-      <img src={Img1} alt="Breed img"/>
+      <img src="https://picsum.photos/200/100" alt="Raza img"/>
     </div>
     <div>
-      <h2>Razas</h2>
+      <h3>Raza de gato</h3>
     </div>
   </div>
+)
+
+export default () => (
+  <>
+    <div className="raza-header">
+      <div>
+        <img src={Img1} alt="Breed img" />
+      </div>
+      <div>
+        <h2>Razas</h2>
+      </div>
+    </div>
+    <div className="raza-body">
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+      <RazaCard />
+    </div>
+  </>
 )
