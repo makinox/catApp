03267501bd37b5dc7f23ed69d5966cd2de4274ba
Jesús Bossa <me@jesusbossa.com>
2018-12-19 19:33:15 -0500

    [IMPROVEMENT] Reducer improved

--- src/utils/redux/reducer/index.js
+++ src/utils/redux/reducer/index.js
@@ -9,34 +9,6 @@ export default function reducef(state, action) {
       }
     }
 
-    case 'INSERT_USER': {
-      return {
-        ...state,
-        user: action.payload.user
-      }
-    }
-
-    case 'INSERT_SUBJECTS': {
-      return {
-        ...state,
-        subject: action.payload.subject
-      }
-    }
-
-    case 'INSERT_NOTES': {
-      return {
-        ...state,
-        note: action.payload.note
-      }
-    }
-
-    case 'INSERT_RATINGS': {
-      return {
-        ...state,
-        rating: action.payload.rating
-      }
-    }
-
     default:
       return state
   }
