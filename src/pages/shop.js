36d0c2c516e141dc9080c5005cec3d44ef30e4d6
Jesús Bossa <me@jesusbossa.com>
2018-12-20 21:38:26 -0500

    [ADD] New page shop

---
+++ src/pages/shop.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Shop from '../components/shop/shop'
+
+export default class extends React.Component {
+  render(){return <Shop />}
+}
--- src/utils/routes/routes.js
+++ src/utils/routes/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from '../../pages/index'
 import Breed from '../../pages/raza'
 import Types from '../../pages/type'
+import Shop from '../../pages/shop'
 
 export default (props) => (
   <Router>
@@ -12,6 +13,7 @@ export default (props) => (
         <Route exact path="/" component={Home} />
         <Route exact path="/breed" component={Breed} />
         <Route exact path="/type" component={Types} />
+        <Route exact path="/store" component={Shop} />
       </Switch>
     </>
   </Router>
