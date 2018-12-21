0110bb9ae58313c72acb5757c75406ba6e42ad9d
Jesús Bossa <me@jesusbossa.com>
2018-12-20 20:23:02 -0500

    [IMPROVEMENT] Type header improvement

--- src/components/types/types.js
+++ src/components/types/types.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import Img1 from '../../assets/types/cat4.svg'
+import './types.css'
 
 export default () => (
-  <h2>Tipos</h2>
+  <>
+    <div className="type-header">
+      <div>
+        <img src={Img1} alt="Breed img" />
+      </div>
+      <div>
+        <h2>Tipos</h2>
+      </div>
+    </div>
+    <div className="type-body">
+
+    </div>
+  </>
 )
