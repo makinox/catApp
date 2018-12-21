00041616a5ab687380076c62be78597f3e31d193
Jesús Bossa <me@jesusbossa.com>
2018-12-20 20:28:10 -0500

    [ADD] Type card added

--- src/components/types/types.js
+++ src/components/types/types.js
@@ -2,6 +2,12 @@ import React from 'react'
 import Img1 from '../../assets/types/cat4.svg'
 import './types.css'
 
+const TypeCard = () => (
+	<div className="types-card">
+		<a href="/">Raza siames <span role="img" aria-label="Emoji section">😜</span></a>
+	</div>
+)
+
 export default () => (
   <>
     <div className="type-header">
@@ -13,7 +19,27 @@ export default () => (
       </div>
     </div>
     <div className="type-body">
-
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
+      <TypeCard />
     </div>
   </>
 )
