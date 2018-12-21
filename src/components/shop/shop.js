2d787e922172365fe060f620a5e7553e9ddd56fa
Jesús Bossa <me@jesusbossa.com>
2018-12-20 21:47:33 -0500

    [IMPROVEMENT] Shop header improved

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import Img1 from '../../assets/shop/cat5.svg'
+import './shop.css'
 
 export default () => (
-  <h1>Hola shop</h1>
+  <>
+    <div className="shop-header">
+      <div>
+        <img src={Img1} alt="Breed img" />
+      </div>
+      <div>
+        <h2>Tienda</h2>
+      </div>
+    </div>
+    <div className="shop-body">
+    </div>
+  </>
 )
