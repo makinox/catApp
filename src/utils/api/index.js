7434b7f10db20ad198838e9f8a3fafb8c107218c
Jesús Bossa <me@jesusbossa.com>
2018-12-21 21:21:13 -0500

    [ADD] Unsplash api

---
+++ src/utils/api/index.js
@@ -0,0 +1,3 @@
+import unsplash from './unsplash/unsplash'
+
+export {unsplash}
---
+++ src/utils/api/unsplash/unsplash.js
@@ -0,0 +1,8 @@
+import Unsplash from 'unsplash-js';
+
+export default new Unsplash({
+  applicationId: "39cd3dbd9cbab7bceca737fcbf274aafabbccae4513a72300dee82eacd8faa70",
+  secret: "bdebc12aa00b352b4f185fa067e162c1438c09adccf96304655ce652c6baba09",
+  callbackUrl: "urn:ietf:wg:oauth:2.0:oob"
+});
+
