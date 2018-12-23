9f55d395382a041d2f732723571a09a7350b336f
Jesús Bossa <me@jesusbossa.com>
2018-12-23 15:51:23 -0500

    [ADD] Magic header added

--- src/components/magia/magia.js
+++ src/components/magia/magia.js
@@ -1,3 +1,19 @@
 import React from 'react'
+import Img1 from '../../assets/magia/cat5.svg'
+import './magia.css'
 
-export default () => <h1>Hola magia</h1>
+export default () => (
+  <>
+    <div className="magia-header">
+      <div>
+        <img src={Img1} alt="Breed img" />
+      </div>
+      <div>
+        <h2>Galeria magica</h2>
+      </div>
+    </div>
+    <div className="magia-body">
+
+    </div>
+  </>
+)
