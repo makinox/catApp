0072821faae36732171bae0a62b36e1a42c91a24
Jesús Bossa <me@jesusbossa.com>
2018-12-25 18:06:14 -0500

    [ADD] Cat api added

--- src/components/magia/magia.js
+++ src/components/magia/magia.js
@@ -2,7 +2,13 @@ import React from 'react'
 import Img1 from '../../assets/magia/cat5.svg'
 import './magia.css'
 
-export default () => (
+const CatImages = (props) => (
+  <div>
+    <img src={props.src} alt={props.alt} />
+  </div>
+)
+
+export default (props) => (
   <>
     <div className="magia-header">
       <div>
@@ -13,7 +19,10 @@ export default () => (
       </div>
     </div>
     <div className="magia-body">
-
+      {console.log(props.cats)}
+      {props.cats.map( (el, i) => (
+        <CatImages src={el.thumb} alt={el.description} key={i}/>
+      ))}
     </div>
   </>
 )
--- src/pages/magia.js
+++ src/pages/magia.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import Magia from '../components/magia/magia'
+import {unsplash} from '../utils/api/index'
 
 export default class extends React.Component {
-  render(){return <Magia />}
+
+  state = {
+    cats: [{}]
+  }
+
+  async componentDidMount() {
+    const res = await unsplash.search.photos("cats", 1, 20)
+    let {results: cats} = await res.json()
+    cats = cats.map(el => ({description: el.description, thumb: el.urls.thumb}))
+    this.setState({cats})
+  }
+
+  render(){return <Magia cats={this.state.cats} />}
 }
